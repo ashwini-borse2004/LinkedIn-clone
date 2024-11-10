867e4a06fb9866b99dccf6d3ff3027595f05c196
ashwini-borse2004 <borseashwini735@gmail.com>
2024-11-11 00:45:53 +0530

    Add files via upload

---
+++ Experience.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+//Experience field Schema
+const ExperienceSchema =new mongoose.Schema({
+  companyName:{
+    type: String,
+    required: true,
+  },
+  position:{
+    type: String,
+    required:true,
+  },
+  startDate:
+  {
+    type: Date,
+    required: false,
+  },
+  endDate:{
+    type: String,
+    required: false,
+  },
+  discription:{
+    type: String,
+    required: false,
+  },
+    });
+
+    const Experience = mongoose.model("Experience", ExperienceSchema);
+    module.exports = Experience; 
