0f5eb1940f9928c2dce6bd0cbeb7f66578aceb2a
Ashwini Borse <borseashwini735@gmail.com>
2024-11-14 23:19:19 +0530

    Update index.js
    Signed-off-by: Ashwini Borse <borseashwini735@gmail.com>

--- LinkedIn-clone-backend/index.js
+++ LinkedIn-clone-backend/index.js
@@ -1,1 +1,47 @@
+const express = require("express");
+const { ExtractJwt, JwtStrategy } = require("passport-jwt");
 
+const app = express();
+
+// passport-jwt setup
+// jwt_payload: {identifier: userId}
+
+let options = {};
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = "thisIsSupposedToBeSecret";
+
+ passport.use
+ (
+    new JwtStrategy(options, function(jwt_payload,done)
+    {
+       User.findOne({_id: jwt_payload.identifier}, function(err, user)
+      {
+          if(err)
+          {
+            done(err , false);
+          }
+          if(user)
+          {
+            done(null,user);
+          }
+          else
+          {
+           done(null,false);
+          }
+       });
+    })
+ );
+
+app.get("/",(req,res)=>
+{
+    res.send("hello world");
+
+});
+app.get("/hello",(req,res)=>
+    {
+        res.send("hello");
+    
+    });
+app.listen(3000,()=>{
+    console.log("server working");
+});
