bd77e8a99e960b65180a31c68a76534eb8f3c74f
anjalikaldate <145894649+anjalikaldate@users.noreply.github.com>
2024-11-16 13:58:13 +0530

    Update authentication.js
    Signed-off-by: anjalikaldate <145894649+anjalikaldate@users.noreply.github.com>

--- LinkedIn-clone-backend/routes/authentication.js
+++ LinkedIn-clone-backend/routes/authentication.js
@@ -43,6 +43,39 @@ router.post("/register", async(req, res) =>
   delete userToReturn.password;
   return res.status(200).json(userToReturn); 
 });
- 
+ router.post("/login", async (req,res)=>{
+   //step 1:we get the details from yhe req.body
+
+   const {email, password}=req.body;
+
+   //step 2: verify if a user exists with that email
+   const user = await User.findOne({email: email});
+   if(!email || !password){
+    return res.status(401).json({err:"Invalid username or password"});
+   }
+   if(!user){
+    return res.status(401).json({err: "Invalid username or password"});
+   }
+
+   //step 3: verify if the password provided by the user for login is correct
+   //this is tricky part
+   //direct password comparison will not work password==user.passwrod
+   const isPasswordValid = await bcrypt.compare(password, user.password);
+   if(!isPasswordValid){
+    return res.status(401).json({err: "Invalid username or password"});
+   }
+
+
+   //step 4: generate a token for the user and return it
+   
+   const token= await getToken(email, newUser);
+   //we want to return following to the userSelect: 
+   //1. the  actual user created
+   //2. the token
+   const userToReturn ={...newUser.toJson(), token};
+   delete userToReturn.password;
+   return res.status(200).json(userToReturn);
+
+});
 
 module.exports=router;
