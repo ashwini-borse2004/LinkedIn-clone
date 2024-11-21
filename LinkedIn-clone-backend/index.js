dea8a3fe31e0feacfb53342a1750dcd45a1c24c9
Ashwini Borse <borseashwini735@gmail.com>
2024-11-21 21:41:49 +0530

    MongoDB connection
    Signed-off-by: Ashwini Borse <borseashwini735@gmail.com>

--- LinkedIn-clone-backend/index.js
+++ LinkedIn-clone-backend/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
-const { ExtractJwt, JwtStrategy } = require("passport-jwt");
+const passport = require("passport");
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const JwtStrategy = require("passport-jwt").Strategy;
+const mongoose = require("mongoose");
 
+require("dotenv").config();
 const app = express();
 
 // passport-jwt setup
-// jwt_payload: {identifier: userId}
+// jwt_payload: {identifier: userId} ${process.env.MONGO_PASSWORD}
+mongoose.connect("mongodb+srv://aborse2004:" + process.env.MONGO_PASSWORD + "@cluster1.i5lp8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology : true,
+    }
+).then((x) => {
+    console.log("Connnected to mongo!");
+})
+.catch((error) => {
+    console.log("Error occured while connecting to mongo");
+    console.log(error);
+
+});
 
 let options = {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
